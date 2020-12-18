463111a1ba3875bf96b1f5a2310ac82b16e56f8c
Wlodzimierz Lipert <wlodzimierz.lipert@gmail.com>
2020-12-18 14:03:09 +0100

    Refactored STATIC_ASSERT to avoid compiler warnings.

--- drv/assert.h
+++ drv/assert.h
@@ -1,25 +1,48 @@
 #pragma once
 
+#include <stddef.h>
 
 #define TO_STR_IMPL(x) #x
 #define TO_STR(x) TO_STR_IMPL(x)
 
+#if defined HOST
+
+#define STATIC_ASSERT(cond)
+#define STATIC_ASSERT_STRUCT_OFFSET(type, field, offset)
+
+#else
+
+#define STATIC_ASSERT_MSG(cond, msg) \
+    typedef struct {uint8_t value : (cond) ? 8 : 0;} static_assert_##msg
+    //typedef char static_assert_##msg[2 * !!(cond) - 1]
+
+#define STATIC_ASSERT_IMPL1(cond, L, C) \
+    STATIC_ASSERT_MSG(cond, C##_at_line_##L)
+
+#define STATIC_ASSERT_IMPL2(cond, L, C) \
+    STATIC_ASSERT_IMPL1(cond, L, C)
+
+#define STATIC_ASSERT(cond) \
+    STATIC_ASSERT_IMPL2(cond, __LINE__, __COUNTER__)
+
+#define STRUCT_OFFSET(type, field) \
+    (size_t)((const uint8_t *const)(&((const type *const)NULL)->field))
+
+#define STATIC_ASSERT_STRUCT_OFFSET(type, field, offset) \
+    STATIC_ASSERT(STRUCT_OFFSET(type, field) == (size_t)(offset))
+    //STATIC_ASSERT((const uint8_t *const)(&((const type *const)NULL)->field) == (const uint8_t *const)(offset));
+
+#endif
 
 #if defined ASSERT_DISABLE || defined HOST
 
 #define ASSERT(cond)
-#define STATIC_ASSERT(cond)
-#define STATIC_ASSERT_STRUCT_OFFSET(type, field, offset)
 
 #else /* ASSERT_DISABLE */
 
 #include <avr/interrupt.h>
 
-void dbg_led_on(void);
-void dbg_led_off(void);
-void dbg_led_toggle(void);
 void sos(const char *message, uint16_t code);
-
 
 #define ASSERT(cond) \
     do \
@@ -46,20 +69,5 @@ void sos(const char *message, uint16_t code);
             SREG = sreg; \
         } \
     } while(0)
-
-#define STATIC_ASSERT_MSG(cond, msg) \
-    typedef char static_assert_##msg[2 * !!(cond) - 1]
-
-#define STATIC_ASSERT_IMPL1(cond, L) \
-    STATIC_ASSERT_MSG(cond, at_line_##L)
-
-#define STATIC_ASSERT_IMPL2(cond, L) \
-    STATIC_ASSERT_IMPL1(cond, L)
-
-#define STATIC_ASSERT(cond) \
-    STATIC_ASSERT_IMPL2(cond, __LINE__)
-
-#define STATIC_ASSERT_STRUCT_OFFSET(type, field, offset) \
-    STATIC_ASSERT((uint8_t *)(&((type *)NULL)->field) == (uint8_t*)offset);
 
 #endif /* ASSERT_DISABLE */

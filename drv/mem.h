2dfcc8af37d848fa83bfc4364019df377d6aaeff
Wlodzimierz Lipert <wlodzimierz.lipert@gmail.com>
2021-08-22 12:54:09 +0200

    Refactor.

--- drv/mem.h
+++ drv/mem.h
@@ -23,12 +23,12 @@
                                 (uintptr_t)(*(const uint8_t *)(addr)) << 8\
                                 | (uintptr_t)(*((const uint8_t *)(addr) + 1))))
 
-#define M1(I1) (1 << I1)
-#define M2(I1, I2) (M1(I1) | 1 << I2)
-#define M3(I1, I2, I3) (M2(I1, I2) | 1 << I3)
-#define M4(I1, I2, I3, I4) (M3(I1, I2, I3) | 1 << I4)
-#define M5(I1, I2, I3, I4, I5) (M4(I1, I2, I3, I4) | 1 << I5)
-#define M6(I1, I2, I3, I4, I5, I6) (M5(I1, I2, I3, I4, I5) | 1 << I6)
-#define M7(I1, I2, I3, I4, I5, I6, I7) (M6(I1, I2, I3, I4, I5, I6) | 1 << I7)
+#define M1(I1) (UINT8_C(1) << I1)
+#define M2(I1, I2) (M1(I1) | UINT8_C(1) << I2)
+#define M3(I1, I2, I3) (M2(I1, I2) | UINT8_C(1) << I3)
+#define M4(I1, I2, I3, I4) (M3(I1, I2, I3) | UINT8_C(1) << I4)
+#define M5(I1, I2, I3, I4, I5) (M4(I1, I2, I3, I4) | UINT8_C(1) << I5)
+#define M6(I1, I2, I3, I4, I5, I6) (M5(I1, I2, I3, I4, I5) | UINT8_C(1) << I6)
+#define M7(I1, I2, I3, I4, I5, I6, I7) (M6(I1, I2, I3, I4, I5, I6) | UINT8_C(1) << I7)
 
 #endif /* mem_h */
